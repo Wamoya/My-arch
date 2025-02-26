6e8c478017cf3e90b3a7a886886816db52f18823
Wamoya <juangrossodll@gmail.com>
2025-02-26 12:52:13 +0100

    Add line for .bash_aliases, also hiding windows user name

--- upload.sh
+++ upload.sh
@@ -8,3 +8,6 @@ cp ~/packages.txt ~/repos/My-arch/
 
 # .bashrc
 cp ~/.bashrc ~/repos/My-arch/dotfiles/bashrc
+
+# .bash_aliases
+sed -E 's|(/mnt/c/Users/)[^/]+|\1{{WIN_USER}}|g' ~/.bash_aliases > ~/repos/My-arch/dotfiles/bash_aliases
