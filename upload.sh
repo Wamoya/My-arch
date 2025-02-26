77e719308420f323393996393bc440ea76d8862c
Wamoya <juangrossodll@gmail.com>
2025-02-26 11:58:27 +0100

    Add line for .bashrc

--- upload.sh
+++ upload.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# Nvim config
 cp --recursive ~/.config/nvim ~/repos/My-arch/configs/
+
+# Package list
 cp ~/packages.txt ~/repos/My-arch/
+
+# .bashrc
+cp ~/.bashrc ~/repos/My-arch/dotfiles/bashrc
