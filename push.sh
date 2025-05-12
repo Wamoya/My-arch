520d8c80dc889a7551dee846264a74f4ce4844ef
Wamoya <juangrossodll@gmail.com>
2025-05-12 19:16:53 +0200

    Add error handling to the push.sh utility script

--- push.sh
+++ push.sh
@@ -2,6 +2,11 @@
 
 msg="Update files"
 
+if [ $# -ne 0 ] && [ $# -ne 1 ]; then
+    echo -e "Usage: $0 <Optional commit message>\n- Default commit message: ${msg}"
+    exit 1
+fi
+
 if [ $# -eq 1 ]; then
     msg="$1"
 fi
